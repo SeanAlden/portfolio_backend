9bbd39b46f520d2e8e4f829b7a9746a2f02e7043
Sean Alden <c14210052@john.petra.ac.id>
2026-06-07 20:02:38 +0700

    update education model

--- app/Models/Education.php
+++ app/Models/Education.php
@@ -6,10 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Education extends Model
 {
+    protected $table = 'educations';
     protected $fillable = [
-        'university', 
-        'degree', 
-        'period', 
+        'university',
+        'degree',
+        'period',
         'gpa'
     ];
 
