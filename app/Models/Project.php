7853a1bb22da47f8b1b3f05e1fb7c098b567aa1b
Sean Alden <c14210052@john.petra.ac.id>
2026-06-07 23:19:32 +0700

    update proejct model

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -6,6 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Project extends Model
 {
+    protected $table = 'projects';
+
     protected $fillable = ['education_id', 'name', 'desc', 'tech', 'github_urls', 'image'];
 
     protected $casts = [
